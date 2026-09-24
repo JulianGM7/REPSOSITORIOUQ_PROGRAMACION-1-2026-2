39b9c99bff848ec60d29919cee94358eba5d475f
Daniela <daniela.lopezl@uqvirtual.edu.co>
2026-09-24 17:53:28 -0500

    Cree la estructura del proyecto y avance en HotelStayPlus

---
+++ src/HotelStayPlus.java
@@ -0,0 +1,32 @@
+package HotelStayPlus;
+
+import java.util.ArrayList;
+
+public class HotelStayPlus {
+
+    private String nombreComercial;
+    private String nit;
+    private String direccion;
+    private String telefono;
+    private String paginaweb;
+
+    private List<Huesped> huespedes;
+    private List<Habitacion> habitaciones;
+    private List<Reserva> reservas;
+    private List<ServicioAdicional> serviciosAdicionales;
+
+    public HotelStayPlus(String nombreComercial, String nit, String direccion, String telefono, String paginaweb) {
+        this.nombreComercial = nombreComercial;
+        this.nit = nit;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.paginaweb = paginaweb;
+
+        this.huespedes = new ArrayList<>();
+        this.habitaciones = new ArrayList<>();
+        this.reservas = new ArrayList<>();
+        this.serviciosAdicionales = new ArrayList<>();
+
+    }
+
+}
