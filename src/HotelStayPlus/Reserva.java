7372f3e85b7f6b4d644d6c7ae5cafb6abba1a042
Daniela <daniela.lopezl@uqvirtual.edu.co>
2026-09-24 19:19:43 -0500

    trabaje en los metodos de Reserva

--- src/HotelStayPlus/Reserva.java
+++ src/HotelStayPlus/Reserva.java
@@ -1,7 +1,6 @@
 package HotelStayPlus;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.List;
 
@@ -29,6 +28,54 @@ public class Reserva {
       this.serviciosAdicionales = new ArrayList<>();
       this.valorTotal =0.0;
 
+      // asignar reserva al huesped
+      if (huesped != null) {
+          huesped.agregarReserva(this);
+       }
+      }
+
+    /**
+     * valida y asigna habitación a la reserva.
+     * @param habitacion agregar
+     * @return true si se agregó correctamente, false si no esta disponible
+     */
+      public boolean agregarHabitacion(Habitacion habitacion) {
+        if (habitacion == null) retun false;
+        if (habitacion.estaDisponible()) {
+            System.out.println("Error: La Habitacón N°" + habitacion.getNumero() + "no se encuentra disponible.");
+            return false;
+        }
+        if (!habitaciones.contains(habitacion)) {
+            habitaciones.add(habitacion);
+            return true;
+        }
+        return false;
   }
+
+    /**
+     * Agregar servicio adicional
+     * @param servicioAdicional
+     * @return true si se agregó, false si no se agregó
+     */
+    public boolean agregarServicioAdicional(ServicioAdicional servicioAdicional) {
+          if (servicio != null && servicio.isDisponible()){
+              servicioAdicionales.add(servicio);
+              recalcularValorTotal();
+              return true;
+          }
+          return false;
+    }
+
+    public int calcularCantidadNoches() {
+        if (fechaEntrada != null && fechaSalida != null) {
+            int noches = fechaSalida.getDayOfMonth() - fechaEntrada.getDayOfMonth();
+            if (noches <= 0) {
+                return 1;
+            }
+            return noches;
+        }
+        return 0;
+    }
+
 
 }
